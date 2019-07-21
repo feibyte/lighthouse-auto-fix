6bc4fb0ad73f35ba869be5e6a5d76b60a6694c2e
fedeoo <fedeoo.zf@gmail.com>
2019-07-21 15:09:07 +1000

    Amend image test

--- test/ImageOptimizer.test.js
+++ test/ImageOptimizer.test.js
@@ -2,7 +2,7 @@
 
 const ImageOptimizer = require('../src/optimizers/ImageOptimizer');
 
-describe('computeResize()', () => {
+describe('ImageOptimizer.computeResize()', () => {
   const naturalWidth = 100;
   const naturalHeight = 200;
   it('should return the original size even when display width is not smaller than the original', () => {
@@ -55,5 +55,53 @@ describe('computeResize()', () => {
     );
     expect(resize.width).toBe(50);
     expect(resize.height).toBe(displayedMaxHeight);
+  });
+});
+
+describe('ImageOptimizer.refine()', () => {
+  const generateImage = props => {
+    return Object.assign(
+      {
+        src: 'https://localhost/logo.png',
+        isCss: false,
+        usesObjectFit: false,
+        displayedWidth: 100,
+        displayedHeight: 200,
+        naturalWidth: 100,
+        naturalHeight: 200,
+      },
+      props
+    );
+  };
+  it('should return correct ', () => {
+    const ImageElements = [
+      generateImage({ displayedWidth: 80, displayedHeight: 120 }),
+      generateImage({ displayedWidth: 60, displayedHeight: 160 }),
+    ];
+    const audits = {
+      'offscreen-images': [],
+      'uses-webp-images': [],
+    };
+    const artifacts = {
+      ImageElements,
+      URL: {
+        finalUrl: 'https://localhost/',
+      },
+    };
+    const images = ImageOptimizer.refine(artifacts, audits);
+    expect(images.length).toBe(1);
+    expect(images).toEqual([
+      {
+        src: 'https://localhost/logo.png',
+        isFromSameSite: true,
+        resizable: true,
+        isOffScreen: false,
+        shouldUseWep: false,
+        resize: {
+          width: 80,
+          height: 160,
+        },
+      },
+    ]);
   });
 });
